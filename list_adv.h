02c12f198ba8876a9800d13f8277c2fdcc558328
Alexander Platonov <alexanderplat757@gmail.com>
2017-02-23 15:57:43 +0300

    Add list advanced

---
+++ list_adv.h
@@ -0,0 +1,29 @@
+#include "list.h"
+class Stack : List
+{
+public:
+	Stack() : List(sizeof(double)) { };
+
+	void   push(double x) { add(&x); };
+	double pop() { double res; take_last(&res); return res; };
+};
+
+class Queue : List
+{
+public:
+	Queue() : List(sizeof(double)) { };
+
+	void   put(double x) { add(&x); };
+	double get() { double res; take_first(&res); return res; };
+};
+
+class Deque : List
+{
+public:
+    Deque() : List(sizeof(double)) { };
+
+	void   put(double x) { add(&x); };
+	double first() { double res; take_first(&res); return res; };
+	double last()  { double res; take_last(&res); return res; };
+
+};
--- main.cpp
+++ main.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
-#include "list.h"
+#include "list_adv.h"
 
 using namespace std;
 
 
 int main()
 {
-    List l(4);
     cout << "Hello world!" << endl;
+    Stack s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    cout << "Stack: ";
+    cout << s.pop() << " ";
+    cout << s.pop() << " ";
+    cout << s.pop() << endl;
+    Queue q;
+    q.put(1);
+    q.put(2);
+    q.put(3);
+    cout << "Queue: ";
+    cout << q.get() << " ";
+    cout << q.get() << " ";
+    cout << q.get() << endl;
+    Deque d;
+    d.put(1);
+    d.put(2);
+    d.put(3);
+    d.put(4);
+    cout << "Dequeue: " << endl;
+    cout << "  First: " << d.first() << endl;
+    cout << "  Last: " << d.last() << endl;
+    cout << "  First: " << d.first() << endl;
+    cout << "  Last: " << d.last() << endl;
     return 0;
 }
