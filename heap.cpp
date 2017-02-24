eaf43492ada67cb70b4a14b458c9d8d0e777440b
Alexander Platonov <alexanderplat757@gmail.com>
2017-02-24 19:56:56 +0300

    Refactor heap, remove offset

--- heap.cpp
+++ heap.cpp
@@ -1,5 +1,5 @@
 #include "heap.h"
-Heap::Segment* Heap::current = nullptr;
+Heap::Segment *Heap::current = nullptr;
 int Heap::segment_size = -1;
 
 void* Heap::get_mem(int size)
@@ -7,30 +7,118 @@ void* Heap::get_mem(int size)
     if (segment_size == -1)
         CreateHeap();
     Segment *cur_segment = current;
+    // find free segment
     while (cur_segment)
     {
         if (cur_segment->size >= size)
         {
+            // find free segment_def
             Segment_def *cur_def = cur_segment->last_segment_def;
+            char *cur_def_start = cur_segment->data + cur_segment->size;
             while (cur_def)
             {
+                cur_def_start -= cur_def->size;
                 if (!cur_def->used && cur_def->size >= size)
-                    return (char*) cur_segment->data + use_segment_def(cur_def, size);
-
+                {
+                    use_segment_def(cur_def, size);
+                    return cur_def_start;
+                }
                 cur_def = cur_def->prev;
             }
         }
         cur_segment = cur_segment->prev;
     }
+    create_new_segment(size);
+    return current->data;
+}
 
+void Heap::free_mem (void *mem)
+{
+    char *chmem = (char*) mem;
+    // find segment containing this mem
+    Segment *cur_segment = current, *next_segment = nullptr;
+    while (cur_segment)
+    {
+        if (chmem >= cur_segment->data &&
+            chmem < cur_segment->data + cur_segment->size)
+            break;
+        next_segment = cur_segment;
+        cur_segment = cur_segment->prev;
+    }
+    if (!cur_segment)
+        return;
+
+    // find segment_def of this mem
+    Segment_def *cur_def = cur_segment->last_segment_def, *next_def = nullptr;
+    char *cur_def_start = cur_segment->data + cur_segment->size - cur_def->size;
+    while (chmem < cur_def_start)
+    {
+        next_def = cur_def;
+        cur_def = cur_def->prev;
+        cur_def_start -= cur_def->size;
+    }
+    if (chmem != cur_def_start)
+        return;
+
+    // union near-placed segment-def's
+    Segment_def *prev_def = cur_def->prev;
+    if (prev_def && !prev_def->used)
+    {
+        cur_def->size += prev_def->size;
+        cur_def->prev = prev_def->prev;
+        delete prev_def;
+    }
+    if (next_def && !next_def->used)
+    {
+        next_def->size += cur_def->size;
+        next_def->prev = cur_def->prev;
+        delete cur_def;
+    }
+    else
+    {
+        cur_def->used = false;
+    }
+
+    // delete empty segment
+    if (cur_def->size == cur_segment->size)
+    {
+        if (next_segment)
+            next_segment->prev = cur_segment->prev;
+        delete cur_def;
+        delete [] cur_segment->data;
+        delete cur_segment;
+    }
+}
+
+
+void Heap::use_segment_def(Segment_def* segment_def, int size)
+{
+    if (size != segment_def->size)
+    {
+        Segment_def* new_segment_def = new Segment_def;
+        new_segment_def->size = size;
+        segment_def->size -= size;
+
+        new_segment_def->prev = segment_def->prev;
+        segment_def->prev = new_segment_def;
+
+        segment_def = new_segment_def;
+    }
+    segment_def->used = true;
+}
+
+void Heap::create_new_segment(int size)
+{
+    // create segment
     int new_segment_size = size > segment_size? size : segment_size;
     Segment* segment = new Segment;
     segment->data = new char[new_segment_size];
+    segment->size = new_segment_size;
     segment->prev = current;
     current = segment;
 
+    // create segment_def's of the segment
     Segment_def* first_def = new Segment_def;
-    first_def->offset = 0;
     first_def->prev = nullptr;
     first_def->size = size;
     first_def->used = true;
@@ -41,106 +129,28 @@ void* Heap::get_mem(int size)
     else
     {
         Segment_def* second_def = new Segment_def;
-        second_def->offset = size;
         second_def->prev = first_def;
         second_def->size = new_segment_size - size;
         second_def->used = false;
         segment->last_segment_def = second_def;
     }
-    return segment->data;
-}
-
-void Heap::free_mem (void* mem)
-{
-    char *chmem = (char*) mem;
-    Segment *cur_segment = current, *next_segment = nullptr;
-    while (cur_segment)
-    {
-        if (chmem >= (char*) cur_segment->data &&
-            chmem < (char*) cur_segment->data + cur_segment->size)
-            break;
-        next_segment = cur_segment;
-        cur_segment = cur_segment->prev;
-    }
-    if (!cur_segment)
-        return;
-
-    char *segment_start = (char*) cur_segment->data;
-    Segment_def *cur_def = cur_segment->last_segment_def, *next_def = nullptr;
-    while (chmem < segment_start + cur_def->offset)
-    {
-        next_def = cur_def;
-        cur_def = cur_def->prev;
-    }
-    if (chmem != segment_start + cur_def->offset)
-        return;
-
-    Segment_def *prev_def = cur_def->prev;
-    if (prev_def && !prev_def->used)
-    {
-        cur_def->offset = prev_def->offset;
-        cur_def->size += prev_def->size;
-        cur_def->prev = prev_def->prev;
-        delete prev_def;
-    }
-    if (next_def && !next_def->used)
-    {
-        next_def->offset = cur_def->offset;
-        next_def->size += cur_def->size;
-        next_def->prev = cur_def->prev;
-        delete cur_def;
-    }
-    else
-    {
-        cur_def->used = false;
-    }
-
-    if (cur_def->size == cur_segment->size)
-    {
-        if (next_segment)
-            next_segment->prev = cur_segment->prev;
-        delete cur_def;
-        delete [] (char*) cur_segment->data;
-        delete cur_segment;
-    }
-}
-
-
-int Heap::use_segment_def(Segment_def* segment_def, int size)
-{
-    if (size != segment_def->size)
-    {
-        Segment_def* new_segment_def = new Segment_def;
-        new_segment_def->size = size;
-        segment_def->size -= size;
-
-        new_segment_def->offset = segment_def->offset;
-        segment_def->offset += size;
-
-        new_segment_def->prev = segment_def->prev;
-        segment_def->prev = new_segment_def;
-
-        segment_def = new_segment_def;
-    }
-    segment_def->used = true;
-    return segment_def->offset;
 }
 
 void Heap::delete_segments()
 {
     while (current)
     {
-        Segment *prev = current->prev;
+        Segment *prev_seg = current->prev;
         Segment_def *cur_def = current->last_segment_def;
         while (cur_def)
         {
-            Segment_def *prev = cur_def->prev;
+            Segment_def *prev_def = cur_def->prev;
             delete cur_def;
-            cur_def = prev;
+            cur_def = prev_def;
         }
-        delete [] (char*) current->data;
+        delete [] current->data;
         delete current;
-        current = prev;
+        current = prev_seg;
     }
 }
 
--- heap.h
+++ heap.h
@@ -13,30 +13,30 @@ public:
         segment_size = _segment_size;
     }
     static void* get_mem (int size);
-    static void free_mem (void* mem);
+    static void free_mem (void *mem);
 private:
     struct Segment_def
     {
         bool used;
         int size;
-        int offset;
         Segment_def *prev;
     };
 
     struct Segment
     {
         int size;
-        void *data;
+        char *data;
         Segment *prev;
         Segment_def *last_segment_def;
     };
 
-    static int use_segment_def(Segment_def* segment_def, int size);
+    static void use_segment_def(Segment_def *segment_def, int size);
+    static void create_new_segment(int size);
     static void delete_segments();
 
     static int segment_size;
 
-    static Segment* current;
+    static Segment *current;
 };
 
 #endif // __HEAP_H_INCLUDED__
