3715be0f513de6a9406eec3ce1e5de316f771a0d
Alexander Platonov <alexanderplat757@gmail.com>
2017-03-12 01:48:28 +0300

    Finish numeric constant parsing

--- LexemeParsing/ConstantLexemeParser.cpp
+++ LexemeParsing/ConstantLexemeParser.cpp
@@ -1,12 +1,13 @@
 #include "LexemeParser.h"
+#include <climits>
 
 bool TryParseAsCharConstant(const char *s, unsigned long &length, LexemeInfo *result, LexemeError *error);
 bool TryParseAsStringConstant(const char *s, unsigned long &length, LexemeInfo *result, LexemeError *error);
-bool TryParseNumConstant(const char *s, unsigned long &length, LexemeInfo *result, LexemeError *error);
-bool TryParseDouble(const char *s, unsigned long &length, double &result, NumType &numType);
-bool TryParseOctal(const char *s, unsigned long &length, double &result);
-bool TryParseHexadecimal(const char *s, unsigned long &length, double &result);
-bool TryGetNum(const char *s, unsigned long &length, double &result, NumType &numType);
+bool TryParseAsNumConstant(const char *s, unsigned long &length, LexemeInfo *result, LexemeError *error);
+bool TryParseDouble(const char *s, unsigned long &length, double &result, NumType &numType, LexemeError *error);
+bool TryParseOctal(const char *s, unsigned long &length, double &result, LexemeError *error);
+bool TryParseHexadecimal(const char *s, unsigned long &length, double &result, LexemeError *error);
+bool TryGetNum(const char *s, unsigned long &length, double &result, NumType &numType, LexemeError *error);
 
 bool isDigit (char ch) { return ch >= '0' && ch <= '9'; }
 bool is_var_char(char ch)
@@ -15,6 +16,7 @@ bool is_var_char(char ch)
 bool TryParseAsConstant(const char *s, unsigned long &length, LexemeInfo *result, LexemeError *error)
 {
     length = 0;
+    error->message = nullptr;
     switch (*s)
     {
         case '\'':
@@ -55,7 +57,7 @@ bool TryParseAsNumConstant(const char *s, unsigned long &length, LexemeInfo *res
 {
     double d = 0;
     NumType numType;
-    if (TryGetNum(s, length, d, numType))
+    if (TryGetNum(s, length, d, numType, error))
     {
         result->type_of_lexeme = NumConstant;
         NumConstantDescription *description = static_cast<NumConstantDescription *>
@@ -74,35 +76,71 @@ bool TryParseAsNumConstant(const char *s, unsigned long &length, LexemeInfo *res
         length++;
     error->error_start = start;
     error->invalid_lexeme = string(s, length);
-    error->message = "Invalid numeric constant";
+    if (error->message != nullptr)
+        error->message = "Invalid numeric constant";
     return false;
     
 }
 
-bool TryGetNum(const char *s, unsigned long &length, double &result, NumType &numType)
+bool TryGetNum(const char *s, unsigned long &length, double &result, NumType &numType, LexemeError *error)
 {
     numType = NumInt;
     if (*s == '0' && isDigit(*(s + 1))) // octal
-        return TryParseOctal(s+1, ++length, result);
-    if (*s == '0' && *(s + 1) == 'x') // hexadecimal
-        return TryParseHexadecimal(s+1, ++length, result);
+        return TryParseOctal(s+1, ++length, result, error);
+    if (*s == '0' && *(s + 1) == 'x' && isDigit(*(s + 2))) // hexadecimal
+        return TryParseHexadecimal(s+2, length += 2, result, error);
     // decimal
-    return TryParseDouble(s, length, result, numType);
+    return TryParseDouble(s, length, result, numType, error);
 }
 
-bool TryParseOctal(const char *s, unsigned long &length, double &result)
+bool TryParseOctal(const char *s, unsigned long &length, double &result, LexemeError *error)
 {
     result = 0;
     while (*s >= '0' && *s <= '7')
     {
         result = result * 8 + *s - '0';
+        if (result > INT_MAX)
+        {
+            error->message = "Too large numeric constant";
+            return false;
+        }
         length++;
         s++;
     }
     return !(is_var_char(*s) || isDigit(*s));
 }
 
-bool TryParseAsDouble(const char *s, unsigned long &length, double &result, NumType &numType)
+int charToHexadecimal(char ch)
+{
+    if (ch >= '0' && ch <= '9')
+        return ch - '0';
+    if (ch >= 'a' && ch <= 'f')
+        return ch - 'a';
+    if (ch >= 'A' && ch <= 'F')
+        return ch - 'A';
+    return -1;
+}
+
+bool TryParseHexadecimal(const char *s, unsigned long &length, double &result, LexemeError *error)
+{
+    result = 0;
+    int curI = charToHexadecimal(*s);
+    while (curI >= 0)
+    {
+        result = result * 16 + curI;
+        if (result > INT_MAX)
+        {
+            error->message = "Too large numeric constant";
+            return false;
+        }
+        curI = charToHexadecimal(*s);
+        length++;
+        s++;
+    }
+    return !(is_var_char(*s) || isDigit(*s));
+}
+
+bool TryParseDouble(const char *s, unsigned long &length, double &result, NumType &numType, LexemeError *error)
 {
     result = 0;
     double floating_part = 0.1;
@@ -122,7 +160,14 @@ bool TryParseAsDouble(const char *s, unsigned long &length, double &result, NumT
                 else if (*s == 'e')
                     curState = 3;
                 else
+                {
+                    if (result > INT_MAX)
+                    {
+                        error->message = "Too large numeric constant";
+                        return false;
+                    }
                     return !is_var_char(*s);
+                }
             break;
             case 1:
                 numType = NumDouble;
@@ -139,6 +184,11 @@ bool TryParseAsDouble(const char *s, unsigned long &length, double &result, NumT
                 if (isDigit(*s))
                 {
                     result = result + floating_part * (*s - '0');
+                    if (floating_part < 0.00000000000001)
+                    {
+                        error->message = "Too small floating part in double constant";
+                        return false;
+                    }
                     floating_part /= 10;
                 }
                 else if (*s == 'e')
@@ -166,12 +216,17 @@ bool TryParseAsDouble(const char *s, unsigned long &length, double &result, NumT
                 }
                 break;
             case 5:
-                if (isDigit(*s))
-                    mantissa = mantissa * 10 + *s - '0';
-                else if (*s == '.' || is_var_char(*s))
+                if (*s == '.' || is_var_char(*s))
                     return false;
                 else
                 {
+                    if (isDigit(*s))
+                        mantissa = mantissa * 10 + *s - '0';
+                    if (mantissa > 14)
+                    {
+                        error->message = "Too large mantissa in double constant";
+                        return false;
+                    }
                     while (mantissa != 0)
                     {
                         if (mantissa_sign > 0)
