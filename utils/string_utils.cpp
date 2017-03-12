d43235f07a173a5251fd8f95bb26d33c4bec8387
Alexander Platonov <alexanderplat757@gmail.com>
2017-03-12 14:25:43 +0300

    Add copy string with length

--- utils/string_utils.cpp
+++ utils/string_utils.cpp
@@ -1,17 +1,25 @@
 #include "../heap/heap.h"
-#include "../heap/heap.h"
 #include <cstring>
+#include "string_utils.h"
 
 char* copy_string(const char *s)
 {
     if (!s)
         return nullptr;
-    int length = 1;
+    unsigned long length = 0;
     const char *temp = s;
     while(*temp++)
         length++;
-    char *result = (char*) Heap::get_mem(length);
+    return copy_string(s, length);
+}
+
+char* copy_string(const char *s, unsigned long length)
+{
+    if (!s)
+        return nullptr;
+    char *result = (char*) Heap::get_mem(length + 1);
     memcpy(result, s, length);
+    result[length] = '\0';
     return result;
 }
 
--- utils/string_utils.h
+++ utils/string_utils.h
@@ -2,7 +2,7 @@
 #define __STRING_HELPERS_H_INCLUDED__
 
 char* copy_string(const char *s);
+char* copy_string(const char *s, unsigned long length);
 bool compare_strings(const char *s1, const char *s2);
-const char* trim_start(const char *s, int &length);
 
 #endif  // __STRING_HELPERS_H_INCLUDED__
