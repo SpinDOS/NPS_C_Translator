90fa458736d1d6df43d23f435477ab9a3485c1ea
murchanskii <fakecatsroad@gmail.com>
2017-03-13 00:59:26 +0300

    Add keyword parser

--- LexemeParsing/KeywordVarNameParser.cpp
+++ LexemeParsing/KeywordVarNameParser.cpp
@@ -3,6 +3,7 @@
 //
 #include "LexemeParser.h"
 #include "../utils/string_utils.h"
+#include "Keywords.h"
 
 bool TryParseAsKeywordOrVariable(const char *s, unsigned long &length, LexemeInfo *result)
 {
@@ -11,15 +12,28 @@ bool TryParseAsKeywordOrVariable(const char *s, unsigned long &length, LexemeInf
         return false;
     for (; *s && (*s >= '0' && *s <= '9') ||
            *s == '_' || (*s <= 'z' &&
-                         *s >= 'a') || (*s <= 'Z' &&
-                                        *s >= 'A'); s++)
+            *s >= 'a') || (*s <= 'Z' &&
+            *s >= 'A'); s++)
     {}
     length = s - start;
     if (length > 0)
     {
-        result -> type_of_lexeme = VariableName;
-        result -> description = copy_string(start, length);
-        return true;
+        char *currentWord = copy_string(start, length);
+        int i = 0, j = 0;
+        while(compare_strings(currentWord, keywords[i][j])){
+            i++;
+            j++;
+        }
+        if (i + j < 77){
+            result -> type_of_lexeme = Keyword;
+            result -> description = (int *) (i + j);
+            return true;
+        }
+        else{
+            result -> type_of_lexeme = VariableName;
+            result -> description = currentWord;
+            return true;
+        }
     }
     else
         return false;
