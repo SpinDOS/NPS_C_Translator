977b9ca3baebe7ccc731a7cf37e6b01e22c5dd1d
murchanskii <fakecatsroad@gmail.com>
2017-03-13 03:12:37 +0300

    Leave function declarations

--- LexemeParsing/Keywords.h
+++ LexemeParsing/Keywords.h
@@ -5,31 +5,7 @@
 #ifndef NPS_C_TRANSLATOR_KEYWORDS_H
 #define NPS_C_TRANSLATOR_KEYWORDS_H
 
-
-const char *keywords[11][7] = {
-        //Loop operators
-        {"for", "while", "do", "break", "continue"},
-        //RETURN
-        {"return"},
-        //IF-ELSE
-        {"if", "else"},
-        //SWITCH-CASE-DEFAULT
-        {"switch", "case", "default"},
-        //True-false
-        {"true", "false"},
-        //Access modifiers
-        {"private", "public", "protected"},
-        //Structure types
-        {"class", "struct", "enum", "namespace"},
-        //Variable types
-        {"void", "char", "bool", "double", "int"},
-        //CONST-STATIC
-        {"const", "static"},
-        //VIRTUAL
-        {"virtual"},
-        //Other functions
-        {"input", "output", "sin", "cos", "max", "min", "new"}
-};
-
+int getKeywordCode(const char *keyword);
+const char *getKeyword(int index);
 
 #endif //NPS_C_TRANSLATOR_KEYWORDS_H
