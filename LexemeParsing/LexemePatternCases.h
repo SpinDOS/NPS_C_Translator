3c39a51e15c4506a848b4a5e5bcb62ad0a7ff297
Alexander Platonov <alexanderplat757@gmail.com>
2017-03-17 03:26:28 +0300

    Move classes for lexeme patterns to separate file

---
+++ LexemeParsing/LexemePatternCases.h
@@ -0,0 +1,73 @@
+//
+// Created by Alexander on 17-Mar-17.
+//
+
+#ifndef NPS_C_TRANSLATOR_LEXEMEPATTERNCASES_H
+#define NPS_C_TRANSLATOR_LEXEMEPATTERNCASES_H
+
+#include "../utils/string_utils.h"
+#include "../heap/heap.h"
+
+class LexemeCase
+{
+public:
+    virtual ~LexemeCase(){};
+    virtual bool IsMyChar(char ch) = 0;
+    virtual int GetNextState() = 0;
+};
+
+class CharsRange
+{
+    char start, end;
+public:
+    CharsRange(char _start, char _end) {start = _start; end = _end; }
+    bool IsMyChar(char ch) { return ch >= start && ch <= end; }
+};
+
+class RangeCase : public LexemeCase
+{
+    CharsRange *range;
+    int newState;
+public:
+    RangeCase(CharsRange *_range, int _newState)
+    { range = _range; newState = _newState; }
+    bool IsMyChar(char ch) override {return range->IsMyChar(ch);}
+    int GetNextState() override {return newState;}
+};
+
+class StringCase : public LexemeCase
+{
+    char *str;
+    int newState;
+public:
+    StringCase(const char *start, unsigned long length, int _newState)
+    {str = copy_string(start, length); newState = _newState;}
+    StringCase(const char *start, int _newState)
+    {str = copy_string(start); newState = _newState;}
+    
+    ~StringCase() override {Heap::free_mem(str);}
+    bool IsMyChar(char ch) override
+    {
+        const char *cur = str;
+        while (*cur && *cur != ch)
+            cur++;
+        return *cur == ch;
+    }
+    int GetNextState() override {return newState;}
+};
+
+class SpecialGroupCase : public LexemeCase
+{
+    int newState;
+public:
+    SpecialGroupCase(int _newState)
+    { newState = _newState; }
+    
+    int GetNextState() override
+    { return newState; }
+    
+    bool IsMyChar(char ch) override
+    { return ch == ' ' || ch == '\r' || ch == '\n' || ch == '\t' || ch == '\v'; }
+};
+
+#endif //NPS_C_TRANSLATOR_LEXEMEPATTERNCASES_H
