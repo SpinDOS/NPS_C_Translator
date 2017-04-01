3fd616b1cfd639673ff3a0e15086a68c8841b961
Alexander Platonov <alexanderplat757@gmail.com>
2017-04-01 03:15:05 +0300

    Add simple linked list

--- NPS_library/collection_containers/TSimpleLinkedList.h
+++ NPS_library/collection_containers/TSimpleLinkedList.h
@@ -5,13 +5,35 @@
 #ifndef NPS_C_TRANSLATOR_SIMPLELINKEDLIST_H
 #define NPS_C_TRANSLATOR_SIMPLELINKEDLIST_H
 
-#include "../heap/heap.h"
-
-template <typename T> class SimpleLinkedList
+template <typename T> class TSimpleLinkedList
 {
+    struct Node
+    {
+        Node *prev, *next;
+        T data;
+    };
+    Node *first, *last;
+    int _count;
+    bool _error;
+    Node* find_node(int index);
 public:
-    int count();
-
+    TSimpleLinkedList(){ first = last = nullptr; _count = 0; _error = false; }
+    ~TSimpleLinkedList(){clear();}
+    int count(){return _count;}
+    bool error(){ return _error;}
+    void add (T data);
+    void insertBefore (T data, int index);
+    void insertAfter (T data, int index);
+    T get(int index);
+    T getFirst();
+    T getLast();
+    T take(int index);
+    T takeFirst();
+    T takeLast();
+    T* toArray(int &length);
+    void clear();
 };
+
+#include "TSimpleLinkedList.tpp"
 
 #endif //NPS_C_TRANSLATOR_SIMPLELINKEDLIST_H
