2233c20c20516e535d1245b0812dc81ff1db0de2
Alexander Platonov <alliotalp@yandex.ru>
2017-04-19 00:59:42 +0300

    Add folder for typeCase manager

---
+++ NPS_Compiler/TypeCast/TypeCastManager.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Alexander on 18-Apr-17.
+//
+
+#include "TypeCastManager.h"
+
+bool TypeCastManager::IsCastAvailable(char *beforeType, char *afterType)
+{
+    return true;
+}
---
+++ NPS_Compiler/TypeCast/TypeCastManager.h
@@ -0,0 +1,14 @@
+//
+// Created by Alexander on 18-Apr-17.
+//
+
+#ifndef NPS_C_TRANSLATOR_TYPECASTMANAGER_H
+#define NPS_C_TRANSLATOR_TYPECASTMANAGER_H
+
+class TypeCastManager
+{
+public:
+    static bool IsCastAvailable(char *beforeType, char *afterType);
+};
+
+#endif //NPS_C_TRANSLATOR_TYPECASTMANAGER_H
