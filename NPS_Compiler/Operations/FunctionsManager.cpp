1be9a8dac25fe588c919a9167e9cd3c230bd089a
Alexander Platonov <alliotalp@yandex.ru>
2017-06-06 02:36:17 +0300

    Add prototype for functions manager

---
+++ NPS_Compiler/Operations/FunctionsManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alexander on 06-Jun-17.
+//
+
+#include "FunctionsManager.h"
---
+++ NPS_Compiler/Operations/FunctionsManager.h
@@ -0,0 +1,20 @@
+//
+// Created by Alexander on 06-Jun-17.
+//
+
+#ifndef NPS_C_TRANSLATOR_FUNCTIONSMANAGER_H
+#define NPS_C_TRANSLATOR_FUNCTIONSMANAGER_H
+
+#include "../TNodes/ResultType.h"
+#include "../TNodes/TNode.h"
+
+using namespace NPS_Compiler;
+
+class FunctionsManager
+{
+public:
+    static bool AddFunction();
+};
+
+
+#endif //NPS_C_TRANSLATOR_FUNCTIONSMANAGER_H
